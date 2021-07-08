92244d65443443a5b4d3d4ea83f974f39d3c1b8b
Jesin Amina <Jesin@Jesins-MacBook-Pro-2.local>
2021-07-08 20:28:30 +0530

    Create home page

--- src/app.js
+++ src/app.js
@@ -1,26 +1,71 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-// DB Config
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const cors = require('cors')
 require('../config/db');
-
-const app = express();
-
 const poll = require('./routes/poll');
-
-// Set public folder
-app.use(express.static(path.join(__dirname, '../public')));
-
-// Body parser middleware
+const app = express()
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Planning Poker',
+    name: 'Jesin'
+  })
+})
+app.get('/list', async(req, res) => {
+  res.render('list', {
+    title: 'Stories',
+    name: 'Jesin',
+    list: [],
+    total:0
+  })
+
+})
 // Enable CORS
 app.use(cors());
 
 app.use('/poll', poll);
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Jesin'
+  })
+})
+
+app.get('/autocomplete', async(req, res) => {
+
+  let data = await purchaseOrderDBService.retrieveDistinctNames()
+  res.send({ data })
+
+});
+
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Jesin',
+    errorMessage: 'Page not found.'
+  })
+})
 
 const port = 3000;
 
